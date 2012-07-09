69a4bd99930514aa6e57f6edc4aeb72dd8ebe3c2
Aaron Bycoffe <bycoffe@gmail.com>
2012-07-09 12:33:19 -0400

    Remove extraneous code

--- pollster/pollster.py
+++ pollster/pollster.py
@@ -93,39 +93,3 @@ class Poll(object):
 
     def __repr__(self):
         return '<Poll: %s (%s - %s)>' % (self.pollster, self.start_date, self.end_date)
-
-
-if __name__ == '__main__':
-    # instantiate
-    pollster = Pollster()
-
-    charts = pollster.charts(topic='obama-job-approval')
-    chart = charts[0]
-    print chart
-    print chart.estimates_by_date()
-
-    # access all charts
-    charts = pollster.charts()
-    for chart in charts:
-        print chart
-
-    # access charts for a specific topic
-    charts = pollster.charts(topic='obama-job-approval')
-    for chart in charts:
-        print chart
-
-    # access the polls for a chart
-    chart = charts[0]
-    print chart
-    polls = chart.polls()
-    for poll in polls:
-        print poll
-
-    # page through polls
-    polls = chart.polls(page=2)
-    for poll in polls:
-        print poll
-
-    poll = polls[0]
-    print poll.questions
-
