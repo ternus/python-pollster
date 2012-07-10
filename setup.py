f3752bc2afae91cabc05017c3b480427f5fdb83c
Aaron Bycoffe <bycoffe@gmail.com>
2012-07-10 10:34:35 -0400

    Build out setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from distutils.core import setup
+import pollster
 
 setup(
     name='Pollster',
-    version='0.1.0',
+    version=pollster.__version__,
     packages=['pollster',],
     license='BSD',
-    long_description="A Python wrapper for the Pollster API",
+    author='Aaron Bycoffe',
+    author_email='bycoffe@huffingtonpost.com',
+    description="A Python wrapper for the Pollster API"
+    long_description="A Python wrapper for the Pollster API"
+    url='https://github.com/huffingtonpost/python-pollster',
+    classifiers=[
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
+                 ],
+
 )
