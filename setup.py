fb0042c362830da9a69786caf560c246bf709c22
Adam Hooper <adam@adamhooper.com>
2016-05-24 12:59:35 -0400

    Fiddle with versioning, for version 0.1.4

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from distutils.core import setup
 import pollster
 
 setup(
-    name='Pollster',
-    version=pollster.__version__,
-    packages=['pollster',],
-    license='BSD',
-    author='Aaron Bycoffe',
-    author_email='bycoffe@huffingtonpost.com',
-    description="A Python wrapper for the Pollster API",
-    long_description="A Python wrapper for the Pollster API",
+    name='pollster',
+    version = pollster.__version__,
+    packages = ['pollster'],
+    description = "A Python wrapper for the Pollster API",
+    long_description = "A Python wrapper for the Pollster API",
+    license = 'MIT',
+    author = 'Aaron Bycoffe',
+    author_email = 'bycoffe@huffingtonpost.com',
     url='https://github.com/huffpostdata/python-pollster',
-    classifiers=[
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: BSD License',
-                 'Natural Language :: English',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python',
-                 'Topic :: Software Development :: Libraries :: Python Modules',
-                 ],
-
+    download_url = 'https://github.com/huffpostdata/python-pollster/tarball/' + pollster.__version__,
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
